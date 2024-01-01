ce7385c2b887587a8ec9006a2a7650cdfb419038
Artur Borecki <me@pufereq.pl>
2024-01-01 23:12:48 +0100

    docs(surface.py): add docstrings to `Surface` and its methods

--- src/core/surfaces/surface.py
+++ src/core/surfaces/surface.py
@@ -8,9 +8,25 @@ import pygame as pg
 
 
 class Surface:
+    """Base class for surfaces.
+
+    A surface is a part of the screen. It can be a scene, a topbar, a button,
+    etc. Each surface has its own class, which inherits from this class.
+    """
     def __init__(self, size: tuple[int, int], pos: tuple[int, int] = (0, 0),
                  *, flags: int = 0, depth: int = 32,
                  masks: tuple[int, int, int, int] | None = None):
+        """Initialize the surface.
+
+        Args:
+            size (tuple[int, int]): Size of the surface.
+            pos (tuple[int, int], optional): Position of the surface.
+            flags (int, optional): Flags for the surface. Defaults to 0.
+            depth (int, optional): Color depth of the surface (bits).
+                Defaults to 32.
+            masks (tuple[int, int, int, int] | None, optional): Color masks for
+                the surface. Defaults to None.
+        """
         self.size = size
         self.width = size[0]
         self.height = size[1]
@@ -22,12 +38,41 @@ class Surface:
         self.surface = pg.Surface(size, flags, depth)
 
     def fill(self, color: tuple[int, int, int]):
+        """Fill the surface with a color.
+
+        Args:
+            color (tuple[int, int, int]): Color to fill the surface with.
+
+        Returns:
+            Rect: The area that was filled.
+        """
         return self.surface.fill(color)
 
     def convert(self, surface: pg.Surface):
+        """Convert the surface to the same pixel format as another surface.
+
+        Args:
+            surface (pg.Surface): Surface to convert to.
+
+        Returns:
+            Surface: The converted surface.
+        """
         return self.surface.convert(surface)
 
     def blit(self, source: pg.Surface, dest: tuple[int, int],
              area: tuple[int, int, int, int] | None = None,
              special_flags: int = 0):
+        """Draw one surface onto another.
+
+        Args:
+            source (pg.Surface): Surface to draw.
+            dest (tuple[int, int]): Position to draw the surface at.
+            area (tuple[int, int, int, int] | None, optional): Area of the
+                source surface to draw. Defaults to None.
+            special_flags (int, optional): Special flags for the blit.
+                Defaults to 0.
+
+        Returns:
+            Rect: The area that was drawn.
+        """
         return self.surface.blit(source, dest, area, special_flags)
