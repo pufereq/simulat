7671f86e6d93f518f7c15681e4ec1ef4401069d0
Artur Borecki <me@pufereq.pl>
2024-01-01 22:56:05 +0100

    feat(topbar.py): add `Topbar` class
    Adds a new `Topbar` class which is a topbar displayed at the top of the window. It contains three subsurfaces: `debug_surface` - used to display the version of the game, `title_surface` - displaying the name of the current Scene and `details_surface` - displaying misc information.

---
+++ src/core/surfaces/topbar.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Topbar module for simulat."""
+
+from __future__ import annotations
+import logging as lg
+
+from .surface import Surface
+
+
+class Topbar(Surface):
+    def __init__(self):
+        from ..game import SIZE
+        self.logger = lg.getLogger(f"{__name__}.{type(self).__name__}")
+        self.logger.debug("Initializing topbar...")
+
+        super().__init__((SIZE[0], 24), (0, 0))
+
+        # init sub-surfaces
+        self._init_sub_surfaces()
+
+    def _init_sub_surfaces(self):
+        self.debug_surface = self.surface.subsurface(
+            (0, 0),
+            (self.width * 0.3, self.height)
+        )
+        self.title_surface = self.surface.subsurface(
+            (self.width * 0.3, 0),
+            (self.width * 0.4, self.height)
+        )
+        self.details_surface = self.surface.subsurface(
+            (self.width * 0.7, 0),
+            (self.width * 0.3, self.height)
+        )
+
+        self.debug_surface.fill((255, 0, 0))
+        self.title_surface.fill((0, 255, 0))
+        self.details_surface.fill((0, 0, 255))
+
