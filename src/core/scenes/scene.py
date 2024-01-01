21c2f2b0d477ac647e706306943735b58a306acf
Artur Borecki <me@pufereq.pl>
2024-01-01 02:30:29 +0100

    feat(scene.py): add `Scene` class
    Adds a `Scene` class. A Scene may be e.g. an options menu, a game map or a main menu for the game. A Scene is a pygame `Surface` which takes up the whole screen.

---
+++ src/core/scenes/scene.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Scene module for simulat."""
+from __future__ import annotations
+
+import pygame as pg
+import logging as lg
+
+
+class Scene:
+    """Base class for scenes.
+
+    A scene is a state of the game, e.g. the main menu, the game itself, the
+    settings menu, etc. Each scene has its own class, which inherits from this
+    class. The scenes are stored in the `scenes` dict in the `Simulat` class.
+    A scene takes up the whole screen.
+    """
+    def __init__(self) -> None:
+        """Initialize the scene."""
+        from ..game import simulat
+        self.id = type(self).__name__
+
+        self.logger = lg.getLogger(f"{__name__}.{self.id}")
+
+        self.logger.debug(f"Initializing scene {self.id}...")
+        self.surface = pg.Surface((pg.display.Info().current_w,
+                                   pg.display.Info().current_h))
+        self.surface.fill((255, 255, 255))
+
+        # show fallback message if Scene called directly
+        if self.id == "Scene":
+            text_surface = simulat.fonts["main"].render("FALLBACK SCENE",
+                                                        True, (255, 0, 0))
+            self.surface.blit(text_surface, (20, 20))
+
+    def draw(self, screen):
+        """Draw the scene to the screen."""
+        screen.blit(self.surface, (0, 0))
