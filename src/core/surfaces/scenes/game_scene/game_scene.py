57159999ba14470384fa0b93a184899d8543111d
Artur Borecki <me@pufereq.pl>
2024-01-22 20:29:18 +0100

    feat(game_scene.py): add `GameScene()` scene class
    Adds `GameScene()` scene class. It contains the game map, a sidebar, etc.

---
+++ src/core/surfaces/scenes/game_scene/game_scene.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Game scene."""
+
+from __future__ import annotations
+
+from ..scene import Scene
+from ....game import simulat
+
+from .game_map import GameMap
+
+
+class GameScene(Scene):
+    """Game scene class.
+
+    The game scene is the scene where the game takes place. It contains the
+    game map, a sidebar, etc.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the game scene."""
+        super().__init__()
+
+        # initialize map
+        self.game_map = GameMap(self)
+        self.surface.blit(self.game_map.surface, self.game_map.pos)
+
