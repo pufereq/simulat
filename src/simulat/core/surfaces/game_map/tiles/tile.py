39612174e6c1b0fc463c2e15eeb17ca75c71c90a
Artur Borecki <me@pufereq.pl>
2024-02-06 22:06:58 +0100

    chore(tile.py): remove the exception if `px` not a multiple of `TILE_SIZE` and return a float

--- src/simulat/core/surfaces/game_map/tiles/tile.py
+++ src/simulat/core/surfaces/game_map/tiles/tile.py
@@ -68,9 +68,6 @@ def tiles_to_px(tiles: int | float) -> int:
     return round(tiles * TILE_SIZE)
 
 
-def px_to_tiles(px: int) -> int:
+def px_to_tiles(px: int) -> float:
     """Convert pixels to tiles."""
-    if px % TILE_SIZE != 0:
-        raise ValueError(f"px ({px}) is not a multiple of TILE_SIZE "
-                         f"({TILE_SIZE})")
-    return px // TILE_SIZE
+    return px / TILE_SIZE
