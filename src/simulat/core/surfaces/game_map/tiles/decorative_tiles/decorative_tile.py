2b07718282a19bb69512c736ddaf050b47d48531
Artur Borecki <me@pufereq.pl>
2024-02-19 20:35:31 +0100

    perf(decorative_tile.py): fill surface only once instead every `draw()` call

--- src/simulat/core/surfaces/game_map/tiles/decorative_tiles/decorative_tile.py
+++ src/simulat/core/surfaces/game_map/tiles/decorative_tiles/decorative_tile.py
@@ -18,10 +18,10 @@ class DecorativeTile(Tile):
         super().__init__(game_map, pos)
 
         self.name = "Decorative Tile"
+        self.surface.fill((10, 128, 10))
 
         self.is_collider = False
 
     def draw(self) -> None:
         """Draw the decorative tile."""
-        self.surface.fill((10, 128, 10))
         super().draw()
